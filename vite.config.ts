1ab0f3076f3c41377a1329909094f653110239db
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-07-09 13:12:38 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,28 +6,33 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  build: {
-    rollupOptions: {
-      output: {
-        entryFileNames: "assets/[hash].js",
-        chunkFileNames: "assets/[hash].js",
-        assetFileNames: "assets/[hash][extname]",
+export default defineConfig(({ command }) => {
+  return {
+    build: {
+      rollupOptions: {
+        output: {
+          entryFileNames: "assets/[hash].js",
+          chunkFileNames: "assets/[hash].js",
+          assetFileNames: "assets/[hash][extname]",
+        },
       },
     },
-  },
-  server: {
-    host: "0.0.0.0",
-    https: {
-      key: fs.readFileSync(path.join(__dirname, "./dev-https/https.key")),
-      cert: fs.readFileSync(path.join(__dirname, "./dev-https/https.cer")),
+    server:
+      command === "serve"
+        ? {
+            host: "0.0.0.0",
+            https: {
+              key: fs.readFileSync(path.join(__dirname, "./dev-https/https.key")),
+              cert: fs.readFileSync(path.join(__dirname, "./dev-https/https.cer")),
+            },
+          }
+        : undefined,
+    envPrefix: ["VITE_", "CF_", "Q_"],
+    plugins: [vue()],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
-  envPrefix: ["VITE_", "CF_", "Q_"],
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
-    },
-  },
+  };
 });

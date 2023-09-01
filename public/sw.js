6e342e580bca1f09d558147476d842cf5c344c29
qiaoshouzi <qiaoshouzi@cfm.moe>
2023-09-01 10:27:14 +0800

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -4,23 +4,23 @@ importScripts("https://storage.googleapis.com/workbox-cdn/releases/7.0.0/workbox
 
 workbox.routing.registerRoute(
   /^https:\/\/dash\.cfm\.moe\/assets\/.+\.(js|css)$/,
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.CacheFirst(),
 );
 
 // assets-dash.cfm.moe/(assets|animeCover)/* 图片等资源
 workbox.routing.registerRoute(
   /^https:\/\/assets-dash\.cfm\.moe\/(assets|animeCover)\/.+$/,
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.CacheFirst(),
 );
 // assets-dash.cfm.moe/dynamic/* 动态信息
 workbox.routing.registerRoute(
   /^https:\/\/assets-dash\.cfm\.moe\/dynamic\/.+$/,
-  new workbox.strategies.StaleWhileRevalidate()
+  new workbox.strategies.StaleWhileRevalidate(),
 );
 // *.hdslb.com BiliBili图片资源
 workbox.routing.registerRoute(
   /^https:\/\/.+\.hdslb\.com\/.+\.(png|jpg|jpeg|svg|gif|webp)$/,
-  new workbox.strategies.CacheFirst()
+  new workbox.strategies.CacheFirst(),
 );
 
 // api-dash.cfm.moe/api/(getAllDynamicData|getConfig|anime) 获取动态信息|获取配置信息|Anime
@@ -28,19 +28,19 @@ workbox.routing.registerRoute(
   /^https:\/\/api-dash\.cfm\.moe\/api\/.+\/?.+token=.+$/,
   new workbox.strategies.NetworkFirst({
     networkTimeoutSeconds: 3,
-  })
+  }),
 );
 
 workbox.routing.registerRoute(
   new RegExp(".*.(?:png|jpg|jpeg|svg|gif|webp)"),
-  new workbox.strategies.StaleWhileRevalidate()
+  new workbox.strategies.StaleWhileRevalidate(),
 );
 workbox.routing.registerRoute(
   new RegExp(".*.(css|js)"),
-  new workbox.strategies.StaleWhileRevalidate()
+  new workbox.strategies.StaleWhileRevalidate(),
 );
 workbox.routing.setDefaultHandler(
   new workbox.strategies.NetworkFirst({
     networkTimeoutSeconds: 3,
-  })
+  }),
 );
